a5ae2b313f2d18da42d0ab6c0e0e372955dbda40
wxy <a1319532999@gmail.com>
2023-09-30 16:26:49 +0800

    first

---
+++ src/main/java/com/example/show2/Show2Application.java
@@ -0,0 +1,13 @@
+package com.example.show2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Show2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Show2Application.class, args);
+    }
+
+}
